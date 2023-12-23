623b47a47613ec1885f17cbc4fb4fb560250b526
Paolo Bursic <paolo6.bursic6@gmail.com>
2023-12-23 13:03:13 +0100

    Login slika dodana

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,20 @@
 
 
 <template>
-  <VApp>
-    <LoginImage/>
-    <VMain></VMain>
-  </VApp> 
+  <v-app>
+    <v-main>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12" md="6">
+                
+            </v-col>
+            <v-col cols="12" md="6">
+            <LoginImage/>
+          </v-col>
+        </v-row>
+    </v-container>
+    </v-main>
+  </v-app> 
 </template>
 
  
--- src/components/LoginImage.vue
+++ src/components/LoginImage.vue
@@ -1,17 +1,8 @@
 <template>
-    <v-container fluid>
-        <v-row>
-            <v-col>
-                
-            </v-col>
-            <v-col cols="12" md="6">
-                <v-img
-                    :aspect-ratio="735/1024"
-                    cover
-                    src="https://aimlief.com/wp-content/uploads/2019/11/featured-tiny-png-101.png"
-                ></v-img>
-            </v-col>
-        </v-row>
-    </v-container>
+    <v-img
+        :aspect-ratio="735/1024"
+        cover
+        src="https://aimlief.com/wp-content/uploads/2019/11/featured-tiny-png-101.png"
+    ></v-img>
 </template>
 
