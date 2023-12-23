1bebf64a157dbbcc061e59da9cec26602271b58e
Paolo Bursic <paolo6.bursic6@gmail.com>
2023-12-23 09:36:54 +0100

    vue instaliran

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 createApp(App).use(router).mount('#app')
