26899bed712400dc75784597daa7737fbc5f9ce3
Paolo Bursic <paolo6.bursic6@gmail.com>
2023-12-29 08:44:59 +0100

    setup za login

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
 
 const routes = [
-  
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/SignUp',
+    name: 'signup',
+    component: Signup
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import Logo from '../components/Logo.vue';
+</script>
+
+<template>
+    <v-app>
+        <Logo></Logo>
+    </v-app>
+</template>
