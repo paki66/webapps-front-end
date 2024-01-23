c09b319a180b054dda194397265981cec5681847
msol0 <147321672+msol0@users.noreply.github.com>
2024-01-23 19:50:08 +0100

    Napravljen nav bar

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-export default {
+import { reactive } from "vue";
+const store = reactive({
   user: {
     id: 1,
     name: "John",
@@ -10,5 +11,21 @@ export default {
     password: "Abc123.?!",
     anniversary: "1.1.2022",
     photo: "https://cdn.vuetifyjs.com/images/profiles/marcus.jpg",
+    status: 0,
   },
-};
+  userStatus: [
+    { status: "Active", icon: "mdi-circle", color: "#00ff44" },
+    { status: "Don't disturb", icon: "mdi-minus-circle-off", color: "#ff0000" },
+    {
+      status: "Out of office",
+      icon: "mdi-arrow-right-circle",
+      color: "#1303fc",
+    },
+    { status: "Be right back", icon: "mdi-account-clock", color: "#fc9003" },
+    { status: "Off sick", icon: "mdi-emoticon-sick", color: "#4d7050" },
+    { status: "Working remotely", icon: "mdi-home", color: "#000000" },
+    { status: "On a vacation", icon: "mdi-airplane", color: "#3189e8" },
+  ],
+});
+
+export default store;
