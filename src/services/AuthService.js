94349144469f3df121c87feaad35fc1facda5af2
msol0 <147321672+msol0@users.noreply.github.com>
2024-04-16 18:20:15 +0200

    dodan setUser u localstorage

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 const dataServiceBaseUrl = "http://localhost:3000/users";
 
+function signup(user) {
+  axios.post(dataServiceBaseUrl + "/signup", user).then((response) => {
+    setUser(response);
+  });
+}
+function setUser(res) {
+  localStorage.setItem("user", JSON.stringify(res.data));
+  return res.data;
+}
 export default {
-  signup(user) {
-    axios.post(dataServiceBaseUrl + "/signup", user).then((response) => {
-      response.data;
-    });
-  },
+  signup,
+  setUser,
 };
