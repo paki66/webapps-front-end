64c2cd60d856f3a8e6313016858f8fb3f9e6d85a
paolo <paolo.bursic@student.unipu.hr>
2024-06-18 19:10:25 +0200

    render

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const userService = axios.create({
-  baseURL: "https://webapps-back-end.vercel.app/users",
+  baseURL: "https://webapps-back-end-1.onrender.com/users",
 });
 
 const projectService = axios.create({
-  baseURL: "https://webapps-back-end.vercel.app/api/projects",
+  baseURL: "https://webapps-back-end-1.onrender.com/api/projects",
 });
 
 const tasksService = axios.create({
-  baseURL: "https://webapps-back-end.vercel.app/api/tasks",
+  baseURL: "https://webapps-back-end-1.onrender.com/api/tasks",
 });
 
 export { userService, projectService, tasksService };
